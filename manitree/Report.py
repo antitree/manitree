f212f8b008cd1a367a0103cfca776c2be0ffc599
AntiTree <antitree@gmail.com>
2011-11-19 17:28:01 -0500

    updated reporting features

--- manitree/Report.py
+++ manitree/Report.py
@@ -24,35 +24,37 @@ def connectSqlite():
 
   try:
     connection = sqlite3.connect("report.db")
+    connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
   except sqlite3.Error, e:
     print("Sqlite error: ", e.args[0])
     sys.exit()
 
+def filterResults(name, value, data):
+  filtered = []
+  for row in data:
+    if row[name] == value:
+      filtered.append(row)
+  return filtered
+
+
 
 class Report:
-  def console(self, mode="text",device=False,package=False,risk=False):
-    connectSqlite()
-    logging.debug("Starting report generation")
-    if device:
-      reportstatement = "SELECT * from report where device='"+device+"'"
-    else:
-      reportstatement = "SELECT * from report"
+  def console(self, data): 
 
-    logging.debug("Sql statement: %s" % reportstatement) 
-    cursor.execute(reportstatement)
+    convert = {'high' : 0, 'medium' : 1, 'low' : 2}
+    data.sort(lambda x, y: cmp(convert[x['risk']],convert[y['risk']]))
 
-    #god. This could be so much better...
     LOW = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDCOLOR = '\033[0m'
-
+  
     print(LOW+'Manitor: Android Manifest Auditing tool')
     print('Author: AntiTree 10/08/2011')
     print('Intrepidus Group')
     print("*" * 50+ENDCOLOR)
-    for row in cursor.fetchall():
+    for row in data:
       if(row[8]) == 'high':
         print(FAIL+row[8].upper()).rjust(1),
       elif(row[8]) == 'medium':
@@ -61,7 +63,100 @@ class Report:
         print(LOW+row[8].upper()).rjust(1),
       print(row[6]+" in "+row[3]+ENDCOLOR).rjust(2)
       print("value:"+row[4]).rjust(3)
-      print(row[7].strip())
-      print("*" * 50)
+  
+  def html(self, data, mode="all", outputFile=False):
+    print("DEVELOPMENT: html mode selected but I haven't made that")
+
+  def text(self, data, outputFile):
+    print("DEVELOPMENT: text file mode selected")
+    convert = {'high' : 0, 'medium' : 1, 'low' : 2}
+    data.sort(lambda x, y: cmp(convert[x['risk']],convert[y['risk']]))
+    deviceSet = set([])
+    packageSet = set([])
+    for row in data:
+      deviceSet.add(row["device"])
+      packageSet.add(row["package"])
     
+    outputtxt = []
+    ##TESTING SUMMARY
+    outputtxt.append('Manitor: Android Manifest Auditing Tool')
+    outputtxt.append("*" * 50)
+    outputtxt.append('Devices tested:')
+    for dev in deviceSet:
+      if not dev == "None": outputtxt.append(dev)
+    outputtxt.append("*" * 50)
+    outputtxt.append('Packages tested:')
+    outputtxt.extend(packageSet)
+    outputtxt.append("*" * 50)
+    ##DETAILS
+    for dev in deviceSet:
+      outputtxt.append("Device Serial: %s" % dev)
+      count = 0
+      for finding in data:
+        if finding["device"] == dev:
+          outputtxt.append("%s: %s in %s" % \
+	    (finding["risk"].upper(), finding["title"], finding["package"]))
+          outputtxt.append("Value: %s" % finding["vulnvalue"])
+          #outputtxt.append(finding["description"]+"\n")
+          count+=1
+      outputtxt.append("Total items found on %s: %i" % (dev, count))
+      outputtxt.append("*" * 50)
+
+    f = open(outputFile, 'w')
+    for line in outputtxt: 
+      f.write("%s\n" % line)
     
+   
+  def devReport(self, device, package=False, risk=False, reportDate=False):
+    logging.debug("Starting device search for %s" % device)
+    #sqlite query for a device
+    connectSqlite()
+    reportstatement = "SELECT * FROM report WHERE device='"+device+"'"
+    logging.debug("Sql statement: %s" % reportstatement)
+    cursor.execute(reportstatement)
+    data = cursor.fetchall()
+    
+    if package: data = filterResults("package", package, data)
+    if risk: data = filterResults("risk", risk, data)
+    if reportDate: data = filterResults("time", reportDate, data)
+    
+    return data
+  
+  def packageReport(self, package, device=False, risk=False, reportDate=False):
+    connectSqlite()
+    reportstatement = "SELECT * FROM  report WHERE package='"+package+"'"
+    cursor.execute(reportstatement)
+    data = cursor.fetchall()
+   
+    if device: data = filterResults("device", device, data)
+    if risk: data = filterResults("risk", risk, data)
+    if reportDate: data = filterResults("time", reportDate, data)
+
+    return data
+  
+  def riskReport(self, risk, device=False, package=False, reportDate=False):
+    connectSqlite()
+    reportstatement = "SELECT * FROM  report WHERE risk='"+risk+"'"  
+    cursor.execute(reportstatement)
+    data = cursor.fetchall()
+   
+    if device: data = filterResults("device", device, data)
+    if package: data = filterResults("package", package, data)
+    if reportDate: data = filterResults("time", reportDate, data)
+
+    return data
+
+  def allReport(self, device=False, package=False, risk=False, reportDate=False):
+    connectSqlite()
+    reportstatement = "SELECT * FROM  report"
+    cursor.execute(reportstatement)
+    data = cursor.fetchall()
+
+    if device: data = filterResults("device", device, data)
+    if package: data = filterResults("package", package, data)
+    if risk: data = filterResults("risk", risk, data)
+    if reportDate: data = filterResults("time", reportDate, data)
+
+    return data
+
+ 

dde197ea457da1f3b83a1d77dc365dd42bbe254d
AntiTree <antitree@gmail.com>
2012-02-05 18:46:17 -0500

    a

---
+++ AxmlParserPY/example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import axmlprinter
+from xml.dom import minidom
+
+def main():
+  ap = axmlprinter.AXMLPrinter(open('test.xml', 'rb').read())
+  buff = minidom.parseString(ap.getBuff()).toxml()
+  print(buff)
+
+if __name__ == "__main__":
+  print("Starting")
+  main()
